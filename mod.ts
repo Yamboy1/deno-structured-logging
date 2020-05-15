1a24a92d1fedca773ceeb5d4595a7a30e8487485
Yamboy1 <yamboyd1@gmail.com>
2020-05-16 09:51:50 +1200

    Add doc comment

--- mod.ts
+++ mod.ts
@@ -7,7 +7,7 @@ import { Sink, consoleSink } from "./sinks.ts";
 export { LogLevel };
 export { Sink, consoleSink };
 
-/* Create a new Logger */
+/** Create a new Logger */
 export function createLogger({ minimumLevel, sinks }: Partial<LoggerOptions> = {}) {
   return new Logger({
     minimumLevel: minimumLevel ?? LogLevel.DEBUG,
