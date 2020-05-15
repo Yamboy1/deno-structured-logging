95c45f4eb2ae450d29d5b10ce5350e470ee3d475
Yamboy1 <yamboyd1@gmail.com>
2020-05-16 10:37:17 +1200

    Refactoring to try and get deno docs to work

--- logger.ts
+++ logger.ts
@@ -1,15 +1,34 @@
 // Copyright 2020 Yamboy1. All rights reserved. MIT license.
 
 import { LogLevel } from "./levels.ts";
-import { Sink } from "./sinks.ts";
+import { Sink, consoleSink } from "./sinks.ts";
 
+
+/** Create a new Logger */
+export function createLogger({ minimumLevel, sinks }: Partial<LoggerOptions> = {}) {
+  return new LoggerImpl({
+    minimumLevel: minimumLevel ?? LogLevel.DEBUG,
+    sinks: sinks ?? [consoleSink],
+  })
+}
 
 export interface LoggerOptions {
   minimumLevel: LogLevel;
   sinks: Sink[]
 }
 
-export class Logger {
+export interface Logger {
+  /** Add a sink at runtime, in most cases, the sinks parameter from createLogger should be used instead. */
+  addSink(sink: Sink): this;
+
+  debug(format: string, ...args: unknown[]): void;
+  info(format: string, ...args: unknown[]): void;
+  warning(format: string, ...args: unknown[]): void;
+  error(format: string, ...args: unknown[]): void;
+  critical(format: string, ...args: unknown[]): void;
+}
+
+class LoggerImpl {
   private minimumLevel: LogLevel;
   private sinks: Sink[] = [];
 
--- mod.ts
+++ mod.ts
@@ -1,16 +1,5 @@
 // Copyright 2020 Yamboy1. All rights reserved. MIT license.
 
-import { LogLevel } from "./levels.ts";
-import { Logger, LoggerOptions } from "./logger.ts";
-import { Sink, consoleSink } from "./sinks.ts";
-
-export { LogLevel };
-export { Sink, consoleSink };
-
-/** Create a new Logger */
-export function createLogger({ minimumLevel, sinks }: Partial<LoggerOptions> = {}) {
-  return new Logger({
-    minimumLevel: minimumLevel ?? LogLevel.DEBUG,
-    sinks: sinks ?? [consoleSink],
-  })
-}
+export { Logger, LoggerOptions, createLogger } from "./logger.ts"
+export { LogLevel } from "./levels.ts";
+export { Sink, consoleSink } from "./sinks.ts";
