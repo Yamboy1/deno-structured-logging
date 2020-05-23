90a0023475318b75bbfb0583e4c92548c9767eec
Yamboy1 <yamboyd1@gmail.com>
2020-05-24 10:29:35 +1200

    Add some basic tests

---
+++ mod_test.ts
@@ -0,0 +1,89 @@
+import { assert, assertEquals } from "https://deno.land/std@0.51.0/testing/asserts.ts";
+import { Spy, spy } from "https://deno.land/x/mock@v0.3.0/spy.ts";
+
+import { createLogger } from "./mod.ts";
+import { SinkFunction, LogEntry, LogLevel } from "./types.ts";
+
+function testingSink(arr: LogEntry[]): SinkFunction {
+  return (entry: LogEntry) => {
+    arr.push(entry);
+  }
+}
+
+Deno.test("addSink", () => {
+  const logger = createLogger({ outputFormat: "[{level}] {message}"});
+  const sink: Spy<void> = spy();
+
+  logger.addSink(sink);
+  logger.info("foo");
+
+  // The sink has been run once
+  assertEquals(sink.calls, [
+    {
+      args: [{
+        level: LogLevel.INFO,
+        format: "foo",
+        message: "foo",
+        formattedMessage: "[INFO] foo",
+        variables: {}
+      }],
+      self: { sinkFunc: sink, outputFormat: "[{level}] {message}"}
+    }
+  ]);
+});
+
+Deno.test("addSink with overriding format", () => {
+  const logger = createLogger({ outputFormat: "[{level}] {message}"});
+  const sink: Spy<void> = spy();
+
+  logger.addSink(sink);
+  logger.addSink(sink, "{message} [{level}]");
+  logger.info("foo");
+
+  // The sink has been run once
+  assertEquals(sink.calls, [
+    {
+      args: [{
+        level: LogLevel.INFO,
+        format: "foo",
+        message: "foo",
+        formattedMessage: "[INFO] foo",
+        variables: {}
+      }],
+      self: { sinkFunc: sink, outputFormat: "[{level}] {message}"}
+    },
+    {
+      args: [{
+        level: LogLevel.INFO,
+        format: "foo",
+        message: "foo",
+        formattedMessage: "foo [INFO]",
+        variables: {}
+      }],
+      self: { sinkFunc: sink, outputFormat: "{message} [{level}]"}
+    }
+  ]);
+});
+
+Deno.test("minimumLevel", () => {
+  const logger = createLogger({ minimumLevel: LogLevel.INFO, outputFormat: "[{level}] {message}"});
+  const sink: Spy<void> = spy();
+
+  logger.addSink(sink);
+  logger.debug("foo");
+  logger.info("foo");
+
+  // The sink has been run once
+  assertEquals(sink.calls, [
+    {
+      args: [{
+        level: LogLevel.INFO,
+        format: "foo",
+        message: "foo",
+        formattedMessage: "[INFO] foo",
+        variables: {}
+      }],
+      self: { sinkFunc: sink, outputFormat: "[{level}] {message}"}
+    }
+  ]);
+})
