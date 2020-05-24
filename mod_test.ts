7d4fe08743c8a0e41098f89fd26ce09703dfe358
Yamboy1 <yamboyd1@gmail.com>
2020-05-25 04:40:21 +1200

    refactor: add tests for format strings

--- mod_test.ts
+++ mod_test.ts
@@ -4,7 +4,7 @@ import {
 } from "https://deno.land/std@0.51.0/testing/asserts.ts";
 import { Spy, spy } from "https://deno.land/x/mock@v0.3.0/spy.ts";
 
-import { createLogger } from "./mod.ts";
+import { createLogger, textFormat, jsonFormat } from "./mod.ts";
 import { SinkFunction, LogEntry, LogLevel } from "./types.ts";
 
 Deno.test("addSink", () => {
@@ -83,6 +83,76 @@ Deno.test("minimumLevel", () => {
         variables: {},
       }],
       self: { sinkFunc: sink, outputFormat: "[{level}] {message}" },
+    },
+  ]);
+});
+
+Deno.test("textFormat", () => {
+  const logger = createLogger({ outputFormat: textFormat });
+  const sink: Spy<void> = spy();
+
+  const NativeDate = globalThis.Date;
+  class FakeDate {
+    toISOString() {
+      return "0000-00-00T-00:00:00.0000Z";
+    }
+  }
+  globalThis.Date = FakeDate as DateConstructor;
+  //
+  // Date is now mocked
+  //
+  logger.addSink(sink);
+  logger.info("foo");
+  //
+  // Date is now restored
+  //
+  globalThis.Date = NativeDate
+
+  assertEquals(sink.calls, [
+    {
+      args: [{
+        level: LogLevel.INFO,
+        format: "foo",
+        message: "foo",
+        formattedMessage: "[0000-00-00T-00:00:00.0000Z INFO] foo",
+        variables: {},
+      }],
+      self: { sinkFunc: sink, outputFormat: textFormat },
+    },
+  ]);
+});
+
+Deno.test("jsonFormat", () => {
+  const logger = createLogger({ outputFormat: jsonFormat });
+  const sink: Spy<void> = spy();
+
+  const NativeDate = globalThis.Date;
+  class FakeDate {
+    toISOString() {
+      return "0000-00-00T-00:00:00.0000Z";
+    }
+  }
+  globalThis.Date = FakeDate as DateConstructor;
+  //
+  // Date is now mocked
+  //
+  logger.addSink(sink);
+  logger.info("{foo}", "bar");
+  //
+  // Date is now restored
+  //
+  globalThis.Date = NativeDate
+
+  assertEquals(sink.calls, [
+    {
+      args: [{
+        level: LogLevel.INFO,
+        format: "{foo}",
+        message: "bar",
+        formattedMessage: '{"message":"bar","level":"INFO","timestamp":"0000-00-00T-00:00:00.0000Z","data":{"foo":"bar"}}',
+        variables: {"foo": "bar"},
+      }],
+      self: { sinkFunc: sink, outputFormat: jsonFormat },
     },
   ]);
 });
